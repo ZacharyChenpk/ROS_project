300ef8df18259764d8aa3205cbd11b83279432e7
ZacharyChenpk <954617373@qq.com>
2019-12-15 21:39:52 +0800

    .

--- src/respawnGoal.py
+++ src/respawnGoal.py
@@ -29,7 +29,7 @@ class Respawn():
     def __init__(self):
         self.modelPath = os.path.dirname(os.path.realpath(__file__))
         self.modelPath = self.modelPath.replace('proj_api/src',
-                                                'turtlebot3_simulations/turtlebot3_gazebo/models/turtlebot3_square/goal_box/model.sdf')
+                                                'proj_api/goal_box/model.sdf')
         self.f = open(self.modelPath, 'r')
         self.model = self.f.read()
         self.stage = rospy.get_param('/stage_number')
